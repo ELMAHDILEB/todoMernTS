06c25f8a9ea4260d49f14787986a5f23deb0c125
ELMAHDILEB <elmahdibelcadi@gmail.com>
2025-07-24 21:27:43 +0000

    create config & models folder and connect with mongodb

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
-import cors from "cors";
-import dotenv from "dotenv"
-const express = require("express");
-
+const dotenv = require("dotenv");
 dotenv.config();
 
+const cors = require("cors");
+const connectDB = require("./config/db.js")
+connectDB();
+
+
+// build express application
+const express = require("express");
 const app = express();
 
+
+// Global middlware
+/***enable CORS for CROSS ORIGIN requests ***/
 app.use(cors());
-app.use(express.json())
+// convert JSON formate to request.body
+app.use(express.json());
 
 
-
+// run the server
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
-    console.log(`server running in ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
